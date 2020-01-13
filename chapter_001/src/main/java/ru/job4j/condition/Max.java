05c88c8d3555731545fbf5f6aa0a90c9f0ba1db0
Sashke1488 <mutsnt1488666@mail.ru>
2020-01-13 15:06:27 +0300

    Изменение в максимум из двух чисел[#216388]

--- chapter_001/src/main/java/ru/job4j/condition/Max.java
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -2,16 +2,15 @@ package ru.job4j.condition;
 
 public class Max {
     public static int maxLeft(int left, int right) {
-        int resultLeft = left > right ? left : right;
+        int resultLeft = left >= right  ? left : right;
+
         return resultLeft;
     }
-    public static  int maxRight(int left, int right){
-        int resultRight = right > left ? right : left;
+
+    public static int maxRight(int left, int right) {
+        int resultRight = right >= left ? right : left;
+
         return resultRight;
     }
 
-    public static  int LeftEqualRight(int left, int right){
-        int resultLeftEqualRight = left == right ? left : right;
-        return resultLeftEqualRight;
-    }
 }
--- chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
@@ -5,20 +5,17 @@ import static org.junit.Assert.assertThat;
 
 public class MaxTest {
     @Test
-    public void whenMax1Left() {
-        int resultLeft = Max.maxLeft(3, 2);
-        assertThat(resultLeft, is(3));
-    }
-    @Test
-    public void whenMax2Right() {
-        int resultRight = Max.maxRight(1, 2);
-        assertThat(resultRight, is(2));
+    public void whenMaxLeft() {
+        int resultLeft = Max.maxLeft(2, 1);
+        assertThat(resultLeft, is(2));
     }
 
     @Test
-    public void whenLeftEqualRight() {
-        int resultLeftEqualRight = Max.LeftEqualRight(3, 3);
-        assertThat(resultLeftEqualRight, is(3));
+    public void whenMaxRight() {
+        int resultRight = Max.maxRight(1, 1);
+        assertThat(resultRight, is(1));
     }
+
+
 
 }
