7869b9410907c03d66a91be2d12048c0ce9be9c2
Sashke1488 <mutsnt1488666@mail.ru>
2020-01-16 20:06:00 +0300

    Отладка программы в IDEA[#216387]

--- chapter_001/src/main/java/ru/job4j/condition/SqMax.java
+++ chapter_001/src/main/java/ru/job4j/condition/SqMax.java
@@ -3,19 +3,23 @@ package ru.job4j.condition;
 public class SqMax {
     public static int max(int first, int second, int third, int forth) {
         int result = forth;
-        if (first > second) {
-            if (first > third) {
-                if (first > forth) {
-                    result = first;
-                }
-            }
+        if (first > second && first > third && first > forth) {
+            result =first;
+
+
         } else if (second > third) {
             if (second > forth) {
                 result = second;
             }
-        } else if (third > forth) {
-            result = third;
-        }
+        } else if (third > forth){
+                    result = third;
+                }
+
+
+
+
+
+
         return result;
     }
-}
+    }
--- chapter_001/src/test/java/ru/job4j/condition/SqMAxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/SqMAxTest.java
@@ -8,22 +8,22 @@ import static org.junit.Assert.assertThat;
 public class SqMAxTest {
     @Test
     public void whenMaxFirst() {
-        int resultLeft = SqMax.max(6,2,3,4);
-        assertThat(resultLeft, is(6));
+        int resultLeft = SqMax.max(6,2,8,4);
+        assertThat(resultLeft, is(8));
     }
     @Test
     public void whenMaxSecond() {
-        int resultLeft = SqMax.max(2,5,3,4);
-        assertThat(resultLeft, is(5));
+        int resultLeft = SqMax.max(7,5,3,4);
+        assertThat(resultLeft, is(7));
     }
     @Test
     public void whenMaxThird() {
-        int resultLeft = SqMax.max(1,3,6,4);
-        assertThat(resultLeft, is(6));
+        int resultLeft = SqMax.max(1,8,6,4);
+        assertThat(resultLeft, is(8));
     }
     @Test
     public void whenMaxForth() {
-        int resultLeft = SqMax.max(2,3,4,7);
-        assertThat(resultLeft, is(7));
+        int resultLeft = SqMax.max(2,3,4,9);
+        assertThat(resultLeft, is(9));
     }
 }
