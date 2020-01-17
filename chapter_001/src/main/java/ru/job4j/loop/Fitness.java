1adc140774c1c96436f1f529b53463538e7bd66f
Sashke1488 <mutsnt1488666@mail.ru>
2020-01-17 19:10:52 +0300

    Протеиновая диета.[#216453]

--- chapter_001/src/main/java/ru/job4j/loop/Fitness.java
+++ chapter_001/src/main/java/ru/job4j/loop/Fitness.java
@@ -3,15 +3,13 @@ package ru.job4j.loop;
 public class Fitness {
     public int calc(int ivan, int nik) {
         int month = 2;
-        while(ivan < nik){
-           for (int i = 0 ; i < month ; i++){
-               ivan *= 3;
-           }
-
-        }
-        while (ivan > nik){
+        while (ivan < nik || ivan > nik) {
+            for (int i = 0; i < month; i++) {
+                ivan *= 3;
+            }
             break;
         }
+
         return month;
     }
 }
