91bc8052605adba23f3790f2dbf742bd2a3487ed
Sashke1488 <mutsnt1488666@mail.ru>
2020-01-19 17:47:51 +0300

    JavaDoc. Code convention[#216461]

---
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -0,0 +1,25 @@
+package ru.job4j.calculate;
+
+/**
+ * Calculate.
+ *
+ * @author aleksandr (mutant1488666@mail.ru)
+ */
+public class Calculate {
+
+    /**
+     * @param args - args.
+     */
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * Method echo.
+     * @param name Your name.
+     * @return Echo plus your name.
+     */
+    public String echo(String name) {
+        return name;
+    }
+}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for calculate task.
+ *
+ * @author Alexander Bautin (mutant1488666@mail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+package ru.job4j.calculate;
