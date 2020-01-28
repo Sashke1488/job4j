4ea6b7109a3213d39f5ecb69674abbc7a6e289e4
Sashke1488 <mutsnt1488666@mail.ru>
2020-01-28 19:18:04 +0300

    Поиск индекса в диапазоне.[#216401]

--- chapter_001/src/main/java/ru/job4j/array/FindLoop.java
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -1,7 +1,7 @@
 package ru.job4j.array;
 
 public class FindLoop {
-    public int indexOf(int[] data, int el) {
+    public static int indexOf(int[] data, int el) {
         int rst = -1;
         for (int i = 0; i < data.length; i++) {
             if (data[i] == el) {
@@ -14,12 +14,12 @@ public class FindLoop {
 
     public static int indexOf(int[] data, int el, int start, int finish) {
         int rst = -1;
-        for (int i = start; i < data.length; i++){
-            if (data[i] == el && data[i] < finish){
-                rst = i;
-                break;
-            }
+        for (int i = start; i < finish; i++) {
+            data[i] = el;
+            rst = i;
+
         }
+
         return rst;
     }
 }
