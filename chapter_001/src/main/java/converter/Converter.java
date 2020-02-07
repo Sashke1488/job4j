9c9c44d189bfeec93d10f43e6a3b95103d756054
Sashke1488 <mutsnt1488666@mail.ru>
2020-02-08 00:37:25 +0300

    6.9. Checkstyle[#216407]

--- chapter_001/src/main/java/converter/Converter.java
+++ chapter_001/src/main/java/converter/Converter.java
@@ -2,8 +2,8 @@ package converter;
 
 public class Converter {
     public static int sqArea(int p, int k) {
-        int h = (p - 2*k)/2;
-        int L = h * k;
-        return L * h;
+        int h = (p - 2 * k) / 2;
+        int l = h * k;
+        return l * h;
     }
 }
