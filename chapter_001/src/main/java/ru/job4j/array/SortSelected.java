eac14eac460cbbd0a87ad8a3c944713120a4eafd
Sashke1488 <mutsnt1488666@mail.ru>
2020-02-14 22:40:30 +0300

    6.8. Дефрагментация массива.[#216405]

--- chapter_001/src/main/java/ru/job4j/array/SortSelected.java
+++ chapter_001/src/main/java/ru/job4j/array/SortSelected.java
@@ -5,14 +5,12 @@ public class SortSelected {
 
     public static int[] sort(int[] data) {
         for (int i = 0; i < data.length; i++) {
-            int min = MinDiapason.findMin(data, 0, data.length);
-            for (int j = 0; j < data.length; j++) {
-                int index = FindLoop.indexOf(data, min, 0, data.length);
-                if (data[i] < data[j]) {
-                    int tmp = data[i];
-                    data[i] = data[j];
-                    data[j] = tmp;
-                }
+            int min = MinDiapason.findMin(data, i, data.length);
+            int index = FindLoop.indexOf(data, min, i, data.length);
+            if (min < data[i]) {
+                int tmp = data[i];
+                data[i] = data[index];
+                data[index] = tmp;
             }
         }
         return data;
