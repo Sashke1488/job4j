3260898530ed9dea3573dd143838509bedb600ce
Sashke1488 <mutsnt1488666@mail.ru>
2020-04-12 14:36:45 +0300

    1.7. Состояние объекта.[#216547]

---
+++ chapter_001/src/main/java/ru/job4j/opp/Battery.java
@@ -0,0 +1,22 @@
+package ru.job4j.opp;
+
+public class Battery {
+    private int load;
+
+    public Battery(int warhead){
+        this.load = warhead;
+    }
+
+    public void exchange(Battery another){
+        this.load = another.load + this.load;
+        another.load = 0;
+    }
+
+    public static void main(String[] args) {
+        Battery first = new Battery(10);
+        Battery second = new Battery(5);
+        System.out.println("first : " + first.load + ". second : " + second.load);
+        first.exchange(second);
+        System.out.println("first : " + first.load + ". second : " + second.load);
+    }
+}
